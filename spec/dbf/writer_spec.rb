b3e695e148ece3a5f1d65e8ee8b6b6b4f3aa85ec
Sven Suska <sven838git@suska.org>
2012-06-01 11:16:00 +0200

    added writer-snippet id=330 v0.1 from http://rubyforge.org/snippet/detail.php?type=snippet&id=253

---
+++ spec/dbf/writer_spec.rb
@@ -0,0 +1,16 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'writer'
+
+module DBF
+  describe Writer do
+    before(:each) do
+      @writer = Writer.new
+    end
+
+    it "should desc" do
+      # TODO
+    end
+  end
+end
