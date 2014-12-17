ac469550e1f4ef9da21d19184aea069c4d757b66
Sven Suska <sven838git@suska.org>
2014-12-17 23:30:36 +0100

    integration of writing capabilities continues: now all writer specs do work

--- lib/dbf/writer.rb
+++ lib/dbf/writer.rb
@@ -22,7 +22,7 @@ end
 
 
 module DBF
-  
+
   # monkeypatching, sorry...
   class Record
     def method_missing(method, *args)
@@ -34,31 +34,31 @@ module DBF
       else
         super
       end
-    end    
+    end
   end
-  
+
   class WriTable < Table
     class InitError < StandardError; end
     class MissingStructureError < StandardError; end
-    
+
     def initialize(file_name_or_stringio, column_defs = nil)
       # if file_name_or_stringio
-      
+
       init_data(file_name_or_stringio)
-      
+
       if @initial_data_present
-        get_header_info 
+        get_header_info
       else
         @version = "03"
         @total_record_count = 0
       end
       @column_defs = column_defs
-            
+
       @records = []
     end
-    
-    def init_data(file_name_or_stringio)      
-      @data = if file_name_or_stringio.is_a?(StringIO) 
+
+    def init_data(file_name_or_stringio)
+      @data = if file_name_or_stringio.is_a?(StringIO)
         @initial_data_present = true
         file_name_or_stringio
       elsif File.exist?(file_name_or_stringio)
@@ -69,28 +69,29 @@ module DBF
         @initial_data_present = false
         raise "File name expected, got #{file_name_or_stringio.inspect}" unless file_name_or_stringio.respond_to?(:upcase)
         File.open(file_name_or_stringio, "w+b")
-      end      
+      end
       @initial_data_present = false if @data.eof?
     end
-    
+
     def has_structure?
-      !!(@column_defs || @header_length)
+      !!(@column_defs || @initial_data_present)
     end
-    
+
     def write(records, column_defs = nil)
 
-      store_column_defs(column_defs)
+      self.column_defs = column_defs if column_defs
+
       @record_count = records.size
       write_header
-      
+
       records.each_with_index do |record, idx|
         next unless record
-        
+
         seek(idx * @record_length)
-        
+
         @data.write(' ')
 
-        @columns.each do |c|
+        columns.each do |c|
           value = record[c.name.to_sym]
 
           if c.type == 'N'
@@ -111,27 +112,28 @@ module DBF
       end
       finish
     end
-    
+
     def close
-      save
+      save if has_structure?
       @data.close && @data.closed?
     end
 
-    def column_defs=(cdefs)
-      if cdefs
-        store_column_defs cdefs
+    def column_defs= cdefs
+      if cdefs != @column_defs or cdefs.nil?
+        @column_defs = cdefs
+        @columns = nil   # invalidate columns cache
       end
-      @column_defs = cdefs
     end
-    
+
     def save
-      write(@records)
+      write( (0...@total_record_count).map {|i| record(i)} )
       @data.flush
       @saved = true
     end
-    
+
     def add_record(attributes={})
       attributes = attributes.attributes if attributes.respond_to?:attributes
+      p attributes
       new_rec = (@record_class||Record).new("", columns, version, false)
       @records[@total_record_count] = new_rec
       new_rec.instance_variable_set( :@attributes, attributes.dup)
@@ -139,24 +141,22 @@ module DBF
       @saved = false
       new_rec
     end
-    
+
     # now cached
     def record(idx)
       @records[idx] ||= super
     end
-    
-  private    
-    def store_column_defs(column_defs = nil)
-      column_defs ||= @column_defs 
-      @columns = if column_defs.nil?
+
+    def columns
+      @columns = if @column_defs.nil?
         if @initial_data_present
-          columns        
+          super
         else
-          raise MissingStructureError, "Cannot write without any column definition"
+          raise MissingStructureError, "Column information needed."
         end
         # Todo: raise better error  if no columns
       else
-        column_defs.map do |c| 
+        @column_defs.map do |c|
           if c.is_a? Column::Base
             c.dup
           else
@@ -164,21 +164,24 @@ module DBF
           end
         end
       end
-      @column_count = @columns.size
+      @columns
     end
-    
 
-    
-    
+    def column_count
+      @columns.size
+    end
+
+  private
+
     def write_header  #(record_count)
       @version = 3
 
       fixed_header_size = 32
-      @header_length = DBF_HEADER_SIZE + @column_count * 32 + 1     # The length of the header
-      @record_length = 1 + @columns.sum{|c|c.length}
-      #@encoding_key 
+      @header_length = DBF_HEADER_SIZE + columns.size * 32 + 1     # The length of the header
+      @record_length = 1 + columns.sum{|c|c.length}
+      #@encoding_key
       #@encoding = ENCODINGS[@encoding_key] if supports_encoding?
-      
+
       now = Time.now()
 
       # Header Info
@@ -197,7 +200,7 @@ module DBF
       # Write out the header
       @data.write(hdr)
 
-      @columns.each do |c|                                  
+      columns.each do |c|
         field = Array.new
         field << c.original_name.ljust(11, "\x00")
         field << c.type[0]
@@ -207,21 +210,21 @@ module DBF
 
         # Write out field descriptor
         @data.write(fld)
-      end      
+      end
 
       # Write terminator '\r'
       @data.write("\r")
-    end # write_header  
-    
+    end # write_header
+
     def finish
       # Write end of file '\x1A'
       @data.write("\x1A")
-      
+
 #      File.open(@file_name, 'w') do |f|
 #        f.write(@data.string)
 #      end
     end
-    
+
   end
 end
 
@@ -231,24 +234,24 @@ end
 #Latest Snippet Version: :0.1
 #id=330	v0.1	2008-01-09 05:22	Joe Lobraco
 
-# A ruby function that can write a basic dbf file. 
-# 
+# A ruby function that can write a basic dbf file.
+#
 # Currently this function only handles text and number fields.
-# 
+#
 # This function takes a filename, a field structure array, and a record array
 # as parameters.
-# 
+#
 # The field structure array is a list of hashes that contain the following:
 #   :field_name - the name of the field (10 characters max)
-#   :field_size - the length of the field (for number fields must be long 
+#   :field_size - the length of the field (for number fields must be long
 #                 enough to hold the string representation of the number)
 #   :field_type - 'N' for number fields (max 18 character length)
 #                 'C' for string fields (max 254 character length)
-#   :decimals   - The number of decimal places for number fields 
+#   :decimals   - The number of decimal places for number fields
 #                 (0 for integers and other data types)
-# 
+#
 #  Example:
-#  
+#
 #  fields = [
 #    {:field_name=>'LOCID', :field_size=>11, :field_type=>'N', :decimals=>0},
 #    {:field_name=>'LOCNAME', :field_size=>254, :field_type=>'C', :decimals=>0},
@@ -257,29 +260,29 @@ end
 #    {:field_name=>'LOCSTATE', :field_size=>2, :field_type=>'C', :decimals=>0},
 #    {:field_name=>'LOCZIP', :field_size=>5, :field_type=>'C', :decimals=>0}
 #  ]
-#  
+#
 #  The record structure array is a list of hashes that contain the data, hashed by
 #  the field name.
-#  
+#
 #  Example:
-#  
+#
 #  records = [
-#   {:LOCID=>1, :LOCNAME=>'My Place', :LOCADDR=>'123 Main St.', 
+#   {:LOCID=>1, :LOCNAME=>'My Place', :LOCADDR=>'123 Main St.',
 #    :LOCCITY=>'Austin', :LOCSTATE=>'TX', :LOCZIP=>'55555'},
-#   {:LOCID=>2, :LOCNAME=>'Your Place', :LOCADDR=>'12 Smith Ave.', 
+#   {:LOCID=>2, :LOCNAME=>'Your Place', :LOCADDR=>'12 Smith Ave.',
 #    :LOCCITY=>'New York', :LOCSTATE=>'NY', :LOCZIP=>'12345'},
 #   {:LOCID=>3, :LOCNAME=>'Their Place', :LOCADDR=>'100 N. High Blvd',
 #    :LOCCITY=>'Atlanta', :LOCSTATE=>'GA', :LOCZIP=>'54321'}
 #  ]
-#  
+#
 # ACKNOWLEDGMENTS:
-# 
-# This code was modeled after the Python DBF Reader and Writer script 
-# written by Raymond Hettinger found on the ASPN website: 
+#
+# This code was modeled after the Python DBF Reader and Writer script
+# written by Raymond Hettinger found on the ASPN website:
 # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/362715
-# 
-# Additionally, information on the dbf file format that was used in 
-# creating this function was found on this website: 
+#
+# Additionally, information on the dbf file format that was used in
+# creating this function was found on this website:
 # http://www.clicketyclick.dk/databases/xbase/format/dbf.html
 #
-		
+
--- spec/dbf/writer_spec.rb
+++ spec/dbf/writer_spec.rb
@@ -7,14 +7,14 @@ require File.expand_path('../../../lib/dbf/writer', __FILE__)
 module DBF
   describe WriTable do
     before(:all) do
-      @dbf_file_name = "#{File.dirname(File.dirname(__FILE__))}/tmp/proba.dbf"      
-      @dbf2_file_name = "#{File.dirname(File.dirname(__FILE__))}/tmp/probb.dbf"      
+      @dbf_file_name = "#{File.dirname(File.dirname(__FILE__))}/tmp/proba.dbf"
+      @dbf2_file_name = "#{File.dirname(File.dirname(__FILE__))}/tmp/probb.dbf"
       @fixt_dbf_simple1_fn = "#{File.dirname(File.dirname(__FILE__))}/fixtures/writer/simple1_6fields3records.dbf"
       @fixt_dbf_simple1_content = File.read(@fixt_dbf_simple1_fn)
       t = Time.now
       @fixt_dbf_simple1_content[1,3] = [t.year-1900, t.month, t.day].pack("CCC")
     end
-    
+
     before(:each) do
       File.delete(@dbf_file_name) if File.exist?(@dbf_file_name)
 
@@ -26,7 +26,7 @@ module DBF
         {:field_name=>'LOCSTATE', :field_size=>20, :field_type=>'C', :decimals=>0},
         {:field_name=>'LOCZIP', :field_size=>20, :field_type=>'C', :decimals=>0}
       ]
-      
+
       @records = [
         {:LOCID=>1,
           :LOCNAME=>'My Place', #ÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP árvíztűrő tükörfúrógép',
@@ -55,7 +55,7 @@ module DBF
           end
         end
       end
- 
+
     end
 
     it "should know about its missing structure non-existing file" do
@@ -71,24 +71,26 @@ module DBF
     it "should create a non-existing dbf-file" do
       @writer = WriTable.new(@dbf_file_name, @fields)
       @writer.has_structure?.should be_true
- 
-      @writer.write(@records)
+
+      @records.each do |r|
+        @writer.add_record(Hash[*r.map{|k,v| [k.to_s.downcase,v]}.flatten])
+      end
       @writer.close
 
       File.read(@dbf_file_name).should == @fixt_dbf_simple1_content
     end
-      
+
     it "should not produce errors with a non-existing file" do
       @writer = WriTable.new(@dbf_file_name, @fields)
       @writer.has_structure?.should be_true
- 
+
       @writer.find(:first, :LOCNAME=>'Blabla').should be_nil
       @writer.close
     end
-      
+
     it "should be able to read columns from an existing file" do
       FileUtils.copy @fixt_dbf_simple1_fn, @dbf_file_name
-      
+
       @writer = WriTable.new(@dbf_file_name)
       @writer.has_structure?.should be_true
       @writer.write(@records)
@@ -99,7 +101,7 @@ module DBF
 
     it "should write the same values that it has read" do
       FileUtils.copy @fixt_dbf_simple1_fn, @dbf_file_name
-      
+
       @writer = WriTable.new(@dbf_file_name)
       recs = @writer.find(:all)
       @writer.write(recs)
@@ -110,12 +112,12 @@ module DBF
 
     it "should copy the same values that it has read" do
       File.delete(@dbf2_file_name) if File.exist?(@dbf2_file_name)
-      
+
       FileUtils.copy @fixt_dbf_simple1_fn, @dbf_file_name
-      
+
       @source = WriTable.new(@dbf_file_name)
       recs = @source.find(:all)
-      
+
       @writer = WriTable.new(@dbf2_file_name)
       @writer.column_defs = @source.columns
       recs.each do |rec|
@@ -124,12 +126,14 @@ module DBF
       @writer.save
       @writer.close
 
+      @source.close
+
       File.read(@dbf2_file_name).should == @fixt_dbf_simple1_content
     end
 
     it "should correctly write modified records" do
       FileUtils.copy @fixt_dbf_simple1_fn, @dbf_file_name
-      
+
       @writer = WriTable.new(@dbf_file_name)
       recs = @writer.find(:all)
       recs[0].attributes["locid"] = 44
